10f78c2ff44634291220f5ccc033eaa3ab2b98a6
andrei <andreilkt@yandex.ru>
2023-07-05 23:23:07 +0700

    yandex

--- test_selenium.py
+++ test_selenium.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("https://google.ru")
+driver.get("https://dzen.ru/")
+search_fields = driver.find_element(By.CLASS_NAME, "dzen-search-arrow-common__frame")
+search_fields.send_keys("Тестирование")
+search_fields.send_keys(Keys.ENTER)
+
+#header = driver.find_element(By.TAG_NAME, "h3")
+#assert header.text == "Тестирование программного обеспечения - Википедия"
+
+
+sleep(5000)
 driver.close()
