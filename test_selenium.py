8908f63fe8ead82531913410f845573fb02b3847
user <andreilkt@yandex.ru>
2023-07-06 16:57:01 +0700

    выполняется запись в поиск

--- test_selenium.py
+++ test_selenium.py
@@ -4,14 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("https://yandex.ru")
-search_fields = driver.find_element(By.ID, "inputWrapper")
+driver.get("https://www.rambler.ru/")
+search_fields = driver.find_element(By.NAME, "query")
 search_fields.send_keys("Тестирование")
 search_fields.send_keys(Keys.ENTER)
 
-#header = driver.find_element(By.TAG_NAME, "h3")
-#assert header.text == "Тестирование программного обеспечения - Википедия"
+header = driver.find_element(By.TAG_NAME, "h3")
+assert header.text == "Тестирование программного обеспечения - Википедия"
 
 
-sleep(5000)
+sleep(10)
 driver.close()
